32b953b7e2981abd0457808fd827182627ee4f3e
Natalia_Kozlikova <kozlikovana@gmail.com>
2020-11-12 20:24:55 +0300

    Homeworks

--- homeworks/les5/task3.py
+++ homeworks/les5/task3.py
@@ -9,19 +9,15 @@ import os
 file_path = os.path.join(os.path.dirname(__file__), 'task3.txt')
 
 with open(file_path, 'r', encoding='UTF-8') as file:
-    line_count = len(file.readlines())
-    file.seek(0)
 
-    idx = 0
-    itm_sum = 0
     print('Фамилии сотрудников c окладом менее 20000 руб.:')
-    while idx <= line_count:
-        my_line = file.readline().split()
+    line_count = 0
+    for line in file:
+        my_line = line.split()
+        my_sal = sum(int(itm) for itm in my_line[1:] if itm.isdigit())
         for itm in my_line:
             if itm.isdigit() and int(itm) < 20000:
                 print((' '.join(my_line)))
-            if itm.isdigit():
-                itm_sum += int(itm)
-        idx += 1
+        line_count += 1
 
-    print(f'Средняя величина дохода сотрудников: {int(itm_sum/line_count)} руб.')
+    print(f'Средняя величина дохода сотрудников: {int(my_sal/line_count)} руб.')
--- homeworks/les5/task4.py
+++ homeworks/les5/task4.py
@@ -17,12 +17,9 @@ file_path_new = os.path.join(os.path.dirname(__file__), 'task4_new.txt')
 num_dict = {1: 'Один', 2: 'Два', 3: 'Три', 4: 'Четыре'}
 
 with open(file_path, 'r', encoding='UTF-8') as file, open(file_path_new, 'w', encoding='UTF-8') as file_new:
-    line_count = len(file.readlines())
-    file.seek(0)
-
     idx = 1
-    while idx <= line_count:
-        my_line = file.readline().split()
+    for line in file:
+        my_line = line.split()
         my_line.remove(my_line[0])
         my_line.insert(0, num_dict[idx])
         file_new.write((' '.join(my_line)) + '\n')
