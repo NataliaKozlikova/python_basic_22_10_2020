fe8fa6737c5b456801e3650b6444526b3b3efa68
Natalia_Kozlikova <kozlikovana@gmail.com>
2020-11-24 21:14:54 +0300

    Homeworks

--- homeworks/les8/task1.py
+++ homeworks/les8/task1.py
@@ -6,6 +6,26 @@
 структуры на реальных данных.
 """
 
+
 class Date:
     def __init__(self, date_str: str):
-        self,date_str = date_str
+        self.date_str = date_str
+
+    @classmethod
+    def date_to_int(cls, date_str):
+        return [int(itm) for itm in date_str.split('-')]
+
+    @staticmethod
+    def date_valid(date_str):
+        date_valid = [int(itm) for itm in date_str.split('-')]
+        # валидация проводится по одному из вариантов для числа, месяца и года
+        if (date_valid[1] not in range(1, 13)) or \
+                (date_valid[0] not in range(1, 32)) or \
+                (date_valid[2] not in range(1, 2021)):
+            return f'Неверный формат даты'
+        else:
+            return f'Дата указана корректно'
+
+
+print(Date.date_to_int('21-12-2020'))
+print(Date.date_valid('01-12-2020'))
